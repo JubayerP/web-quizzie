fc1b55986ed27514a5e4142d0bd36ae774d153ca
JubayerDev <ajashes858@gmail.com>
2022-11-20 22:07:13 +0600

    navbar and routes added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
-import logo from './logo.svg';
+
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './App.css';
+import ErrorElement from './components/ErrorElement/ErrorElement';
+import Home from './components/Home/Home';
+import Main from './layouts/Main';
 
 function App() {
+  const router = createBrowserRouter([
+    {
+      path: '/',
+      errorElement: <ErrorElement />,
+      element: <Main />,
+      children: [
+        {
+          path: '/',
+          element: <Home />
+        },
+        {
+          path: '/home',
+          element: <Home />
+        }
+      ]
+    }
+  ])
   return (
     <div>
-      
+      <RouterProvider router={router}/>
     </div>
   );
 }
---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Link, NavLink } from 'react-router-dom';
+
+const Navbar = () => {
+    return (
+        <div className='bg-[#222222] h-24 flex justify-between items-center px-20'>
+            <div>
+                <Link to='/'><h3 className="text-3xl text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600 font-bold">Web Quizzie</h3></Link>
+            </div>
+            <div className='text-white font-semibold space-x-10'>
+                <NavLink
+                    className={({isActive}) => isActive ? `text-[#00eda4] border-b-2 border-green-500 duration-300`:'hover:text-green-400 duration-300'}
+                    to='/home'>Home</NavLink>
+                <NavLink
+                    className={({isActive}) => isActive ? `text-[#00eda4] border-b-2 border-green-500 duration-300`:'hover:text-green-400 duration-300'}
+                    to='/topics'>Topics</NavLink>
+                <NavLink
+                    className={({isActive}) => isActive ? `text-[#00eda4] border-b-2 border-green-500 duration-300`:'hover:text-green-400 duration-300'}
+                    to='/statistics'>Statistics</NavLink>
+            </div>
+        </div>
+    );
+};
+
+export default Navbar;
