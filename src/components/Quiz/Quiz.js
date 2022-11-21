a9252d953ddb4300e710cd958be38c92d148ee74
JubayerDev <ajashes858@gmail.com>
2022-11-21 13:52:54 +0600

    correct ans added

--- src/components/Quiz/Quiz.js
+++ src/components/Quiz/Quiz.js
@@ -1,5 +1,6 @@
-import React from "react";
+
 import toast from "react-hot-toast";
+import {FiEye,FiEyeOff} from 'react-icons/fi'
 
 const Quiz = ({ quiz }) => {
     const correctAnswer = quiz.correctAnswer;
@@ -13,14 +14,18 @@ const Quiz = ({ quiz }) => {
             toast.error("It's Not Correct!")
         }
     }
+              
 
   return (
-    <div className="border">
-      <h1 className="text-2xl font-semibold">{quiz.question}</h1>
+    <div className="container mx-auto mt-4 shadow-md rounded-md bg-green-600 border p-10">
+          <div className="flex justify-between">
+          <h1 className="text-2xl font-semibold text-yellow-50">{quiz.question}</h1>
+              <FiEye onClick={()=>toast(`Correct Answer: ${correctAnswer}`)} size={30} color='white' className='cursor-pointer'/>
+          </div>
       {quiz.options.map((op,index) => (
-        <div key={index}>
+        <div key={index} className='p-4 space-x-2'>
               <input onClick={()=>handleCorrectAns(op)} type="radio" name="question" id="question" value={op} />
-              <label htmlFor="question">{op}</label>
+              <label className="text-yellow-200 font-semibold text-xl" htmlFor="question">{op}</label>
         </div>
       ))}
     </div>
